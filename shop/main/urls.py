aa50190e2f491e096b2476b8fd10360890a36814
artsiom <a-kondratovich@mail.ru>
2022-04-12 11:41:42 +0300

    3

--- shop/main/urls.py
+++ shop/main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 
-from main.views import hello
-
+from .views import main_page, new_user, Login, Logout
 app_name = 'main'
 urlpatterns = [
-    path('', hello, name='hello'),
+    path('', main_page, name='hello'),
+    path('registration/', new_user, name='reg'),
+    path('logout/', Logout.as_view( ), name='logout'),
+    path('login/', Login.as_view(), name='login'),
 ]
--- shop/main/views.py
+++ shop/main/views.py
@@ -1,7 +1,33 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView, LogoutView, logout_then_login
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.urls import reverse_lazy
 
-def hello(request):
-    return HttpResponse('hello')
+from .forms import NewUserForm
+
+
+def main_page(request):
+    return render(request, 'main_page.html')
+
+
+def new_user(request):
+    if request.method == 'POST':
+        user = NewUserForm(request.POST)
+        if user.is_valid():
+            new_user = User.objects.create_user(**user.cleaned_data)
+            return redirect('main:hello')
+    else:
+        form = NewUserForm()
+        context = {'form': form}
+        return render(request, 'registration.html', context)
+
+class Login(LoginView):
+    template_name = 'Login.html'
+    def get_success_url(self):
+        return reverse_lazy('main:hello')
+
+class Logout(LogoutView):
+    next_page = "main:hello"
